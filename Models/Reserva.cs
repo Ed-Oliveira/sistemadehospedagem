d4059e8469b9963826389c62f2a9d9742717bf57
Edvaldo Oliveira <edjrsantana131@gmail.com>
2023-05-30 13:51:29 -0300

    Sistema de hospedagem de um hotem em C#

---
+++ Models/Reserva.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_Hotel.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+                throw new Exception("A capacidade da suíte é menor do que o numero de hóspedes recebidos!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+           int quantidadeHospedes = Hospedes.Count();
+           return quantidadeHospedes;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0M;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+                valor = (valor * 10) / 100;
+                return valor;
+            }
+            else
+            {
+            return  valor = DiasReservados * Suite.ValorDiaria;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using projeto_Hotel.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hospedes e cadastra na lista de hospedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Carlos");
+Pessoa p2 = new Pessoa(nome: "Maria");
+
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+//Cria a suite
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 10);
+
+//Cria uma nova reserva passando a suite e os hospedes
+Reserva reserva = new Reserva(diasReservados: 100);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+//Exibe a quantidade de hospedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor da diária: {reserva.CalcularValorDiaria():C}");
